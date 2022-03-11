3ba1716ab22c44e95b081c77fa18f6e6d70b810d
VIVEK <VIVEK@DESKTOP-FUABKC3>
2022-03-11 21:10:32 +0530

    Create org test commit

---
+++ src/test/java/practice/DemoGit.java
@@ -0,0 +1,19 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class DemoGit 
+{
+	@Test
+	public void demoGit()
+	{
+		System.out.println("Vivek first commit");
+		
+		
+		
+		
+		
+		
+	}
+
+}
